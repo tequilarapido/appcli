dabfc65ea09cacb411a0049b9a02eb8cc7ba0a46
Nassif Bourguig <nbourguig@gmail.com>
2014-03-06 13:01:46 +0000

    Stopping tests if mailcatcher is not running

--- tests/functional/_bootstrap.php
+++ tests/functional/_bootstrap.php
@@ -1,12 +1,41 @@
 <?php
 
-// Autoload
 if (!defined('TEQ_PROJECT_ROOT')) {
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Autoload
+    |--------------------------------------------------------------------------
+    |
+    */
     define('TEQ_PROJECT_ROOT', realpath(__DIR__ . '/../../'));
     require_once TEQ_PROJECT_ROOT . '/src/vendor/autoload.php';
 
-    // Environnement variables from .env file ?
+    /*
+    |--------------------------------------------------------------------------
+    |   Environnement variables from .env file ?
+    |--------------------------------------------------------------------------
+    |
+    */
     if (is_file(TEQ_PROJECT_ROOT . '/.env')) {
         Dotenv::load(TEQ_PROJECT_ROOT);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Mailcatcher check
+    |--------------------------------------------------------------------------
+    | Make sure mailcatcher is running before trying to launch test
+    | as it is required
+    |
+    */
+    $mailcatcher = new Guzzle\Http\Client('http://127.0.0.1:1080');
+    try {
+        $mailcatcher->get('/messages')->send();
+    } catch (\Exception $e) {
+        $msg = "\n\n\n!!! Error !!!";
+        $msg .= "\nYou need to launch mailcatcher to be able to run tests.";
+        $msg .= "\nOriginal exception message : " . $e->getMessage();
+        die($msg);
     }
 }
